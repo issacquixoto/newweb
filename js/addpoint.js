f5d8fdd9ef4f321155d7d8fbbf3b90b0ed7c2d83
Issac <issac@smartshanghai.com>
2017-08-10 18:17:43 +0800

    更新进度，加了//种族等级数据 ，增加了额外修正选项，C3的刷新页面数据还未完成。

--- js/addpoint.js
+++ js/addpoint.js
@@ -1,26 +1,146 @@
 $(document).ready(function () {
+    //加点
     var characterPoint = {
-        'body': 3,
-        'sta': 3,
-        'int': 3,
-        'feel': 3,
-        'will': 3
+        body: 3,
+        sta: 3,
+        int: 3,
+        feel: 3,
+        will: 3
     };
+    //剩余点数
     var overCharacterPoint;
+    //种族修正
+    var raceCorrection = {};
+    //额外修正
+    var otherRaceCorrection = {};
+    //加点合计
+    var totalCharacterPoint = {};
+    //所选种族
     var changeRace;
+    //种族等级数据
     var raceRank = {
-        'a1': 26,
-        'a2': 26,
-        'b1': 24,
-        'b2': 24,
-        'b3': 24,
-        'b4': 24,
-        'c1': 22,
-        'c2': 22,
-        'c3': 22,
-        'c4': 22,
-        'd1': 20,
-        'd2': 20
+        a1: {
+            point: 26,
+            correction: {
+                body: 2,
+                sta: 2,
+                int: 0,
+                feel: 0,
+                will: 1
+            }
+        },
+        a2: {
+            point: 26,
+            correction: {
+                body: 0,
+                sta: 0,
+                int: 2,
+                feel: 0,
+                will: 1
+            }
+        },
+        b1: {
+            point: 24,
+            correction: {
+                body: 0,
+                sta: 0,
+                int: 1,
+                feel: 0,
+                will: 2
+            }
+        },
+        b2: {
+            point: 24,
+            correction: {
+                body: 1,
+                sta: 1,
+                int: 0,
+                feel: 1,
+                will: 0
+            }
+        },
+        b3: {
+            point: 24,
+            correction: {
+                body: 1,
+                sta: 1,
+                int: 0,
+                feel: 0,
+                will: 0
+                //自选一项+1
+            }
+        },
+        b4: {
+            point: 24,
+            correction: {
+                body: 1,
+                sta: 0,
+                int: 0,
+                feel: 2,
+                will: 0
+            }
+        },
+        c1: {
+            point: 22,
+            correction: {
+                body: -1,
+                sta: -1,
+                int: 1,
+                feel: 0,
+                will: 1
+            }
+        },
+        c2: {
+            point: 22,
+            correction: {
+                body: 0,
+                sta: 0,
+                int: 1,
+                feel: 1,
+                will: -1
+            }
+        },
+        c3: {
+            point: 22,
+            correction: {
+                body: 0,
+                sta: 0,
+                int: 0,
+                feel: 0,
+                will: 0
+            }
+            //自选合计+2并自选一项-1 或自选一项+1
+        },
+        c4: {
+            point: 22,
+            correction: {
+                body: 1,
+                sta: 1,
+                int: -1,
+                feel: 1,
+                will: -1
+            }
+        },
+        d1: {
+            point: 20,
+            correction: {
+                body: 0,
+                sta: 0,
+                int: 0,
+                feel: 0,
+                will: 0
+            }
+        },
+        d2: {
+            point: 20,
+            correction: {
+                body: -1,
+                sta: -1,
+                int: -1,
+                feel: 2,
+                will: -1
+            }
+        }
     };
 
     //选择种族
@@ -29,53 +149,96 @@ $(document).ready(function () {
 
         //导入种族点数
         changeRace = $(characterRace).find("option:selected").val();
-        $('#racePoint').text(raceRank[changeRace]);
+        $('#racePoint').text(raceRank[changeRace].point);
+
+        //导入种族修正
+        raceCorrection = raceRank[changeRace].correction;
+
+        //额外修正开关控制
+        $('#otherRaceCorrection > div').css('display', 'none');
+        if (changeRace === 'c3') {
+            $('#otherRaceCorrection .c3Choose').css('display', 'block');
+        }
+        else if (changeRace === 'b3') {
+            $('#otherRaceCorrection .other01').css('display', 'block');
+        }
 
         OverCharacterPoint();
         refreshValue();
     });
 
-    //按加
+    //加属性点
     $('#characterPoint input[value="+"]').on('click', function () {
         var addPoint = $(this).attr('class');
         //属性点点不够的警报
         if (overCharacterPoint <= 0) {
             $('.error_04').css('display', 'block');
-            return
+            returnamend
         }
         //未选种族则默认为吸血鬼
         if (changeRace === undefined) {
-            changeRace = 'a1'
+            changeRace = 'a1';
+            raceCorrection = raceRank.a1.correction;
         }
         if (characterPoint[addPoint] < 6) {
             ++characterPoint[addPoint];
+            $('.error_01').css('display', 'none');
+            $('.error_02').css('display', 'none');
+
             OverCharacterPoint();
             refreshValue();
-            $('.error_01').css('display', 'none');
         }
         else {
             $('.' + addPoint + 'Point .error_02').css('display', 'inline-block')
         }
     });
 
-    //按减
+    //减属性点
     $('#characterPoint input[value="-"]').on('click', function () {
         var minusPoint = $(this).attr('class');
         //消除点数用完的警报
         $('.error_04').css('display', 'none');
         //未选种族则默认为吸血鬼
         if (changeRace === undefined) {
-            changeRace = 'a1'
+            changeRace = 'a1';
+            raceCorrection = raceRank.a1.correction;
         }
         if (characterPoint[minusPoint] > 3) {
             --characterPoint[minusPoint];
+            $('.error_01').css('display', 'none');
+            $('.error_02').css('display', 'none');
+
             OverCharacterPoint();
             refreshValue();
-            $('.error_02').css('display', 'none');
         }
         else {
             $('.' + minusPoint + 'Point .error_01').css('display', 'inline-block')
         }
+    });
+
+    //额外修正选项
+    $('#otherRaceCorrection input').on('change', function () {
+        var name = $(this).attr('name');
+        var other = $('input[name="' + name + '"]:checked').val();
+        if (otherRaceCorrection[name] === other) {
+            return
+        }
+        else {
+            otherRaceCorrection[name] = other;
+        }
+        if (name === 'c3Choose' && otherRaceCorrection.c3Choose === 'add1') {
+            $('#otherRaceCorrection > div').css('display', 'none');
+            $('#otherRaceCorrection .c3Choose').css('display', 'block');
+            $('#otherRaceCorrection .other01').css('display', 'block');
+            return
+        }
+        if (name === 'c3Choose' && otherRaceCorrection.c3Choose === 'add3') {
+            $('#otherRaceCorrection > div').css('display', 'block');
+            return
+        }
+
+        OverCharacterPoint();
+        refreshValue();
     });
 
     //重置属性点数
@@ -95,7 +258,7 @@ $(document).ready(function () {
         $.each(characterPoint, function (key, val) {
             addCharacterPoint = addCharacterPoint + val
         });
-        overCharacterPoint = raceRank[changeRace] - addCharacterPoint
+        overCharacterPoint = raceRank[changeRace].point - addCharacterPoint
         $('#overRacePoint').text(overCharacterPoint);
     }
 
@@ -103,6 +266,15 @@ $(document).ready(function () {
     function refreshValue() {
         $.each(characterPoint, function (key, val) {
             $('.' + key + 'Point .point').html(val);
+            if (key === otherRaceCorrection.other01) {
+                $('.' + key + 'Point .amend').html(raceCorrection[key] + 1);
+                totalCharacterPoint[key] = raceCorrection[key] + val + 1;
+            }
+            else {
+                $('.' + key + 'Point .amend').html(raceCorrection[key]);
+                totalCharacterPoint[key] = raceCorrection[key] + val;
+            }
+            $('.' + key + 'Point .total').html(totalCharacterPoint[key]);
         });
     }
 });
